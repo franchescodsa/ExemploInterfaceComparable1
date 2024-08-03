d8fa184da04319dac96bdf841f4d6c924ccf9c63
franchesco aprigio <franchescodsa@outlook.com>
2024-08-03 19:33:01 -0300

    Adicionar arquivos de projeto.

---
+++ ExemploInterfaceComparable/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ExemploInterfaceComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            /* Problema motivador
+Faça um programa para ler um arquivo contendo nomes de pessoas (um nome por 
+linha), armazenando-os em uma lista. Depois, ordenar os dados dessa lista e mostra-los 
+ordenadamente na tela. Nota: o caminho do arquivo pode ser informado "hardcode".
+             */
+            string path = @"f:\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> list = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(sr.ReadLine());
+                    }
+                    list.Sort();
+                    foreach (string str in list)
+                    {
+                        Console.WriteLine(str);
+
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
